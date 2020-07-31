e003a5f89ff51965d6b124570c338b9e954ab5d9
A1exShap <alexshaposhnikoff@mail.ru>
2020-07-31 20:35:22 +0400

    ServerChat and ServerUser

---
+++ Chat/ServerUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+
+
+namespace Chat
+{
+    public class ServerUser
+    {
+        #region Properties
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext Context { get; set; }
+
+        #endregion
+    }
+}
--- Chat/ServiceChat.cs
+++ Chat/ServiceChat.cs
@@ -7,11 +7,50 @@ using System.Text;
 
 namespace Chat
 {
-    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class ServiceChat : IServiceChat
     {
-        public void DoWork()
+
+        public List<ServerUser> _users = new List<ServerUser>();
+        private int nextID = 0;
+
+        public int NextID
         {
+            get => ++nextID;
+        }
+
+        public int Connect(string name)
+        {
+            var user = new ServerUser()
+            {
+                ID = NextID,
+                Name = name,
+                Context = OperationContext.Current
+            };
+
+            _users.Add(user);
+
+            SendMessage($"Welcome to our chat, {user.Name}!");
+            return user.ID;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = _users.FirstOrDefault(x => x.ID == id);
+            if (user == null) return;
+            SendMessage($"Good bye, {user.Name}!");
+            _users.Remove(user);
+        }
+
+        public void SendMessage(string message, int senderID = 0)
+        {
+            var senderName = senderID == 0 ?
+                "SERVER" : _users.FirstOrDefault(x => x.ID == senderID).Name;
+
+            var msg = $"({DateTime.Now.ToShortTimeString()})[{senderName}]: {message}";
+
+            foreach (var user in _users)
+                user.Context.GetCallbackChannel<IServerChatCallback>().MessageCallback(msg);
         }
     }
 }
